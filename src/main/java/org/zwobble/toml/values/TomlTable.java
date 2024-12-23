661e69a30cec46fdf2ef85daa63015a6894f0480
Michael Williamson <mike@zwobble.org>
2024-12-23 23:45:04 +0000

    Add basic Java tests for parsing bools

--- src/main/java/org/zwobble/toml/values/TomlTable.java
+++ src/main/java/org/zwobble/toml/values/TomlTable.java
@@ -2,6 +2,7 @@ package org.zwobble.toml.values;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 
 public final class TomlTable implements TomlValue, Iterable<TomlKeyValuePair> {
     public static TomlTable of(List<TomlKeyValuePair> pairs) {
@@ -17,5 +18,25 @@ public final class TomlTable implements TomlValue, Iterable<TomlKeyValuePair> {
     @Override
     public Iterator<TomlKeyValuePair> iterator() {
         return this.pairs.iterator();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TomlTable that = (TomlTable) o;
+        return Objects.equals(pairs, that.pairs);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pairs);
+    }
+
+    @Override
+    public String toString() {
+        return "TomlTable(" +
+            "pairs=" + pairs +
+            ')';
     }
 }
--- src/test/java/org/zwobble/toml/TomlParserTests.java
+++ src/test/java/org/zwobble/toml/TomlParserTests.java
@@ -1,4 +1,36 @@
 package org.zwobble.toml;
 
+import org.junit.jupiter.api.Test;
+import org.zwobble.toml.values.TomlBool;
+import org.zwobble.toml.values.TomlKeyValuePair;
+import org.zwobble.toml.values.TomlTable;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TomlParserTests {
+    @Test
+    public void valueTrue() throws IOException {
+        var result = parse("x = true");
+
+        assertEquals(result, TomlTable.of(List.of(
+            TomlKeyValuePair.of("x", new TomlBool(true))
+        )));
+    }
+
+    @Test
+    public void valueFalse() throws IOException {
+        var result = parse("x = false");
+
+        assertEquals(result, TomlTable.of(List.of(
+            TomlKeyValuePair.of("x", new TomlBool(false))
+        )));
+    }
+
+    private TomlTable parse(String text) throws IOException {
+        return TomlParser.parseReader(new StringReader(text));
+    }
 }
